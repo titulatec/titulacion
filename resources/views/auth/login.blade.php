9ea68dbc4ea5efe8199c77ea0e77f84e6a093431
w2mjim <36895204+w2mjim@users.noreply.github.com>
2018-05-02 14:40:31 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,11 +17,11 @@
                 <div class="panel-heading">Inicio de Sesión</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route($ruta) }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{$nombre}}</label>
+                            <label for="email" class="col-md-4 control-label">E-mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
