1156accb118904e9e30478317fbf55018da9e922
Jose Jaime <w2mjim@gmail.com>
2018-05-02 19:09:00 -0500

    imagen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
             html, body {
                 height: 100%;
                 width: 100%;
-                background: url('images/fondo.jpg');
+                background-image: url('../images/fondo.jpg');
                 background-origin: content-box;
                 background-attachment: relative;
                 background-position: center center;
