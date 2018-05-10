b2ef5871bfac2dcf34ed0860c02c0caf56c3c923
araceliher <39130718+araceliher@users.noreply.github.com>
2018-05-10 11:00:06 -0500

    tabla solicitud

---
+++ database/migrations/2018_05_10_154918_create_solicituds_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolicitudsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //agregar todos los datosde la tabla
+    public function up()
+    {
+        Schema::create('solicituds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('no_control')->unique();
+            $table->string('optitulacion');
+            $table->string('producto'); 
+            $table->string('solicitud');
+            $table->string('tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicituds');
+    }
+}
