196d8b7585bb93047e76759023c02059a14db1e0
Jose Jaime <w2mjim@gmail.com>
2018-05-09 20:29:34 -0500

    Cambios nuevos

--- routes/web.php
+++ routes/web.php
@@ -5,37 +5,54 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+| Aqui se subiran todas las rutas por favor verificar que no tengas errores,
+| solo subir a GitHub cuando no les marque errores en caso de un error grave
+| avisar al equipo.
 */
+
+// Rutas Generales ******* En caso de mover algo favor de avisar en el grupo antes de subirlas a GitHub cada integrante respete su bloque de codigo****
 
 Route::get('/', function () {
     return view('index');
 });
 
-// soy java 100% real no fake, ya ahora si enserio
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('notes', 'NotesController@index');
-Route::get('notes/{id}/destroy', 'NotesController@destroy')->name('notes.destroy');
+//Bloque para Alma ============================================================
+
+
+//============================================================================
+
+//Bloque para Araceli ==========================================================
+
+
+//===========================================================================
+
+//Bloque para Eric ==========================================================
+
+
+//===========================================================================
+
+//Bloque para Mariela ==========================================================
+
+
+//===========================================================================
+
+//Bloque para Jaime==========================================================
 
 Route::get('/alumno', 'AlumnoController@index');
+
 Route::get('/admin', 'AdminController@index');
-Route::get('/div_estudios', 'divisionController@index');
 
 Route::get('/alumno/veralumnos', 'AlumnoController@veralumnos')->name('veralumnos');
 
 Route::post('/registro', 'AdminController@agregar_personal');
 
-Route::get('/subir', function () {
-    return view('admin.subir_alum');
-});
-
 Route::post('/cargar_datos_usuarios', 'AdminController@cargar_datos_usuarios');
 
 Route::post('/registrar_proyecto', 'AlumnoController@registrar_proyecto');
+//===========================================================================
+
+
