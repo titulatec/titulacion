91347d10835dbd0b06cc5145520890f439221420
kimpark12 <40221404+kimpark12@users.noreply.github.com>
2018-06-19 15:04:00 -0500

    LAURA

---
+++ app/Http/Controllers/AnaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Alumno;
+
+use Barryvdh\DomPDF\Facade as PDF;
+
+class AnaController extends Controller
+{
+    public function index()
+    {
+        $products = Alumno::all();
+
+        return view('layouts.ana', compact('products'));
+    }
+
+    public function pdf()
+    {        
+        /**
+         * toma en cuenta que para ver los mismos 
+         * datos debemos hacer la misma consulta
+        **/
+        $products = Alumno::all(); 
+
+        $pdf = PDF::loadView('pdf.products', compact('products'));
+
+        return $pdf->download('listado.pdf');
+    }
+
+
+}
