0f0629212a6ec1134d8e6c6511ea41fb9febef96
Jose Jaime <w2mjim@gmail.com>
2018-06-19 14:30:08 -0500

    Vista

---
+++ resources/views/layouts/jaime.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')<br><br><br>
+    <div class="container col-md-10 col-md-offset-1">
+    <div class="panel panel-primary">
+                <div class="panel-heading">Listado de alumnos</div>
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>No. Control</th>
+                <th>Nombre</th>
+                <th>Apellido Paterno</th>
+                <th>Apellido Materno</th>
+            </tr>                            
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+            <tr>
+                <td>{{ $product->no_control }}</td>
+                <td>{{ $product->nombre }}</td>
+                <td>{{ $product->apellido_p }}</td>
+                <td>{{ $product->apellido_m}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <hr>
+    <p>
+        <a href="{{ route('jaime-alumnos.pdf') }}" class="btn btn-sm btn-primary">
+            Descargar productos en PDF
+        </a>
+    </p>
+
+</div>
+
+</div>
+
+
+
+
+@endsection
